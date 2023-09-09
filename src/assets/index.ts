97df062f8886ec052c6eadd9264efd82ee6e4ee0
Omar Ramoun <contact@omarramoun.com>
2023-09-09 22:51:21 +0300

    feat(assets): add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,225 @@
+/* eslint-disable import/order */
+// SVGs
+import accident from './svg/accident.svg';
+import add from './svg/add.svg';
+import alert from './svg/alert.svg';
+import apple from './svg/apple.svg';
+import back from './svg/back.svg';
+import backButton from './svg/back-button.svg';
+import calendar from './svg/calendar.svg';
+import bookings from './svg/bookings.svg';
+import addClosedDay from './svg/add-closedday.svg';
+import clock from './svg/clock.svg';
+import camera from './svg/camera.svg';
+import addPhoto from './svg/add-photo.svg';
+import paperclip from './svg/paperclip.svg';
+import circle from './svg/circle.svg';
+import clearCross from './svg/clear-cross.svg';
+import comment from './svg/comment.svg';
+import cross from './svg/cross.svg';
+import deleteIcon from './svg/delete.svg';
+import deleteTwo from './svg/delete-two.svg';
+import down from './svg/down.svg';
+import download from './svg/download.svg';
+import edit from './svg/edit.svg';
+import editPencil from './svg/edit-pencil.svg';
+import ellipsis from './svg/ellipsis.svg';
+import feed from './svg/feed.svg';
+import file from './svg/file.svg';
+import filter from './svg/filter.svg';
+import general from './svg/general.svg';
+import hideSidebar from './svg/hide-sidebar.svg';
+import home from './svg/home.svg';
+import hourglass from './svg/hourglass.svg';
+import key from './svg/key.svg';
+import lockedKey from './svg/locked-key.svg';
+import lightning from './svg/lightning.svg';
+import plus from './svg/plus.svg';
+import potty from './svg/potty.svg';
+import profile from './svg/profile.svg';
+import rooms from './svg/rooms.svg';
+import search from './svg/search.svg';
+import settings from './svg/settings.svg';
+import share from './svg/share.svg';
+import showSidebar from './svg/show-sidebar.svg';
+import smallArrow from './svg/small-arrow.svg';
+import smallerArrow from './svg/smaller-arrow.svg';
+import staff from './svg/staff.svg';
+import student from './svg/student.svg';
+import thumbsUp from './svg/thumbs-up.svg';
+import tick from './svg/tick.svg';
+import up from './svg/up.svg';
+import userProfile from './svg/user-profile.svg';
+import zzz from './svg/zzz.svg';
+import drink0 from './svg/drink-0.svg';
+import drink1 from './svg/drink-1.svg';
+import drink2 from './svg/drink-2.svg';
+import drink3 from './svg/drink-3.svg';
+import drink4 from './svg/drink-4.svg';
+import food0 from './svg/food-0.svg';
+import food1 from './svg/food-1.svg';
+import food2 from './svg/food-2.svg';
+import food3 from './svg/food-3.svg';
+import food4 from './svg/food-4.svg';
+import arrowLeft from './svg/arrow-left.svg';
+import arrowRight from './svg/arrow-right.svg';
+import openEye from './svg/open-eye.svg';
+import playIcon from './svg/play-icon.svg';
+import closedEye from './svg/closed-eye.svg';
+import report from './svg/report.svg';
+import inbox from './svg/inbox.svg';
+import time from './svg/time.svg';
+import unreadMessage from './svg/unread-message.svg';
+import questionMark from './svg/question-mark.svg';
+import pause from './svg/pause.svg';
+import fullscreen from './svg/fullscreen.svg';
+import YellowStar from './svg/yellow-star.svg';
+import BlueStar from './svg/blue-star.svg';
+import info from './svg/info.svg';
+import alertTriangle from './svg/alert-triangle.svg';
+import filterEssentials from './svg/filter-essentials.svg';
+import settingsEssentials from './svg/settings-essentials.svg';
+import lock from './svg/lock.svg';
+import actionEdit from './svg/acction-edit.svg';
+import actionNotes from './svg/action-notes.svg';
+import actionPreview from './svg/action-preview.svg';
+import actionSelectAll from './svg/action-select-all.svg';
+import actionSend from './svg/action-send.svg';
+import dragdrop from './svg/dragdrop.svg';
+import expand from './svg/expand.svg';
+import collapse from './svg/collapse.svg';
+import newActivityAccident from './svg/newActivity-accident.svg';
+import newActivityFood from './svg/newActivity-food.svg';
+import newActivityGeneral from './svg/newActivity-general.svg';
+import newActivityLD from './svg/newActivity-ld.svg';
+import newActivityNap from './svg/newActivity-nap.svg';
+import newActivityToileting from './svg/newActivity-toileting.svg';
+import absentIconDefault from './svg/absent-icon-default.svg';
+import absentIconHoliday from './svg/absent-icon-holiday.svg';
+import absentIconUnwell from './svg/absent-icon-unwell.svg';
+import checkInIcon from './svg/check-in-icon.svg';
+import checkOutIcon from './svg/check-out-icon.svg';
+import registerInIcon from './svg/register-in-icon.svg';
+import registerMyDayIcon from './svg/register-my-day-icon.svg';
+import registerDueIcon from './svg/register-due-icon.svg';
+import download2 from './svg/download2.svg';
+import weekly from './svg/weekly-icon.svg';
+// PNGs
+import LogoImage from './ns-logo-image.png';
+import LogoText from './ns-logo-text.png';
+import Clipboard from './empty-state/clipboard.png';
+import ClosedDayCalendarPattern from './closed-day-calendar-pattern.png';
+
+export const iconMap = {
+  pause,
+  fullscreen,
+  accident,
+  add,
+  alert,
+  apple,
+  back,
+  'back-button': backButton,
+  calendar,
+  bookings,
+  'add-closedday': addClosedDay,
+  clock,
+  camera,
+  'add-photo': addPhoto,
+  circle,
+  'clear-cross': clearCross,
+  comment,
+  cross,
+  delete: deleteIcon,
+  'delete-two': deleteTwo,
+  down,
+  download,
+  edit,
+  'edit-pencil': editPencil,
+  ellipsis,
+  feed,
+  file,
+  filter,
+  general,
+  'hide-sidebar': hideSidebar,
+  home,
+  hourglass,
+  key,
+  lightning,
+  plus,
+  potty,
+  profile,
+  rooms,
+  search,
+  settings,
+  share,
+  'show-sidebar': showSidebar,
+  'small-arrow': smallArrow,
+  'smaller-arrow': smallerArrow,
+  staff,
+  student,
+  'thumbs-up': thumbsUp,
+  tick,
+  up,
+  'user-profile': userProfile,
+  zzz,
+  food0,
+  food1,
+  food2,
+  food3,
+  food4,
+  drink0,
+  drink1,
+  drink2,
+  drink3,
+  drink4,
+  paperclip,
+  'play-icon': playIcon,
+  'arrow-left': arrowLeft,
+  'arrow-right': arrowRight,
+  'closed-eye': closedEye,
+  'open-eye': openEye,
+  'yellow-star': YellowStar,
+  'blue-star': BlueStar,
+  report,
+  inbox,
+  'unread-message': unreadMessage,
+  'locked-key': lockedKey,
+  'question-mark': questionMark,
+  info,
+  time,
+  'alert-triangle': alertTriangle,
+  'filter-essentials': filterEssentials,
+  'settings-essentials': settingsEssentials,
+  lock,
+  'action-edit': actionEdit,
+  'absent-icon-default': absentIconDefault,
+  'absent-icon-holiday': absentIconHoliday,
+  'absent-icon-unwell': absentIconUnwell,
+  'check-in-icon': checkInIcon,
+  'check-out-icon': checkOutIcon,
+  'action-notes': actionNotes,
+  'action-preview': actionPreview,
+  'action-select-all': actionSelectAll,
+  'action-send': actionSend,
+  'drag-drop': dragdrop,
+  expand,
+  collapse,
+  'new-activity-accident': newActivityAccident,
+  'new-activity-food': newActivityFood,
+  'new-activity-general': newActivityGeneral,
+  'new-activity-ld': newActivityLD,
+  'new-activity-nap': newActivityNap,
+  'new-activity-toileting': newActivityToileting,
+  'register-in-icon': registerInIcon,
+  'register-my-day-icon': registerMyDayIcon,
+  'register-due-icon': registerDueIcon,
+  download2,
+  weekly,
+};
+
+export const pngMap = {
+  LogoImage,
+  LogoText,
+  Clipboard,
+  ClosedDayCalendarPattern,
+};
