00cd94f117aafae5cd5ad887af3458ad24b7b1f5
Omar Ramoun <contact@omarramoun.com>
2023-09-10 05:40:17 +0300

    feat(components): refactor flex

--- src/components/flex.tsx
+++ src/components/flex.tsx
@@ -3,6 +3,7 @@ import type {ReactNode} from 'react';
 import {View} from 'react-native';
 
 import styled from 'styled-components/native';
+import type {DefaultTheme} from 'styled-components/native';
 import type {BackgroundProps, ColorProps, FlexboxProps, LayoutProps, MarginProps, PaddingProps} from 'styled-system';
 import {background, color, flexbox, layout, margin, padding} from 'styled-system';
 
@@ -15,6 +16,7 @@ export interface FlexProps
     BackgroundProps,
     MarginProps {
   children?: ReactNode;
+  color?: keyof DefaultTheme['colors'];
 }
 
 const Flex = styled(View)<FlexProps>`
