75e9e126df6170727881281d1181a7d3b1eb1878
Omar Ramoun <contact@omarramoun.com>
2023-09-11 22:45:51 +0300

    feat(components): add root index interface to the module

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,61 @@
-export * from './flex';
 export * from './typography';
+export * from './box';
+export * from './flex';
+export * from './router';
+export * from './line';
+export * from './touchable-opacity';
+export * from './touchable-without-feedback';
+export * from './pressable';
+export * from './collapsible';
+export * from './expandable-item';
+export * from './image';
+export * from './empty-state';
+export * from './confirm-box';
+export * from './keyboard-avoiding-view';
+export * from './tag';
+export * from './box-action';
+export * from './center';
+export * from './steps';
+export * from './steps-observation';
+export * from './visual-select';
+export * from './status';
+export * from './info-text';
+
+export * from './button/button';
+export * from './button/button-interaction';
+
+export * from './button-group/button-group';
+export * from './button-group/button-group-item';
+
+export * from './icon';
+export * from './avatar/avatar';
+export * from './avatar/avatar-base64';
+
+export * from './lists/flat-list';
+export * from './lists/section-list';
+
+export * from './spinner/spinner';
+export * from './spinner/spinner-overlay';
+
+export * from './table/table-header';
+export * from './table/table-footer';
+export * from './table/table-body';
+export * from './table/table-item';
+export * from './table/table';
+
+export * from './action-sheet/action-sheet';
+export * from './action-sheet/action-sheet-container';
+
+export * from './modal/modal';
+export * from './modal/modal-content';
+export * from './modal/full-modal';
+export * from './modal/standard-modal';
+export * from './modal/modal-container';
+
+export * from './toast/toast-container';
+export * from './toast/toast-context';
+
+export * from './tabs/tabs';
+export * from './tabs/tabs-pages';
+
+export * from './transition/transition-container';
