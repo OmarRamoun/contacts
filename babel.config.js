0725cb90a1ef8fe91fdbe7e729df79d16c98758f
Omar Ramoun <contact@omarramoun.com>
2023-09-12 06:10:00 +0300

    build: add babel, lint-staged, package.json, and yarn.lock

--- babel.config.js
+++ babel.config.js
@@ -2,38 +2,40 @@ module.exports = {
   presets: [
     'module:metro-react-native-babel-preset',
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        "targets": {
-          "node": "current"
-        }
-      }
+        targets: {
+          node: 'current',
+        },
+      },
     ],
-    "@babel/preset-typescript",
-    "@babel/preset-react"
+    '@babel/preset-typescript',
+    '@babel/preset-react',
   ],
   plugins: [
-    "@babel/plugin-syntax-jsx",
-    ["styled-components", { "ssr": true, "displayName": true }],
-    ["module-resolver",
+    '@babel/plugin-syntax-jsx',
+    ['styled-components', {ssr: true, displayName: true}],
+    [
+      'module-resolver',
       {
-        root: ["./src"],
-        extensions: [".ios.ts", ".android.ts", ".ts", ".ios.tsx", ".android.tsx", ".tsx", ".jsx", ".js", ".json"],
+        root: ['./src'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
         alias: {
-          "@screens": "./src/screens",
-          "@data": "./src/data",
-          "@assets": "./src/assets",
-          "@components": "./src/components",
-          "@constants": "./src/constants",
-          "@hooks": "./src/hooks",
-          "@interfaces": "./src/interfaces",
-          "@layouts": "./src/layouts",
-          "@pages": "./src/pages",
-          "@shared": "./src/shared",
-          "@styles": "./src/styles",
-          "@utils": "./src/utils"
-        }
-      }
-    ]
-  ]
+          '@screens': './src/screens',
+          '@data': './src/data',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@interfaces': './src/interfaces',
+          '@lib': './src/lib',
+          '@layouts': './src/layouts',
+          '@pages': './src/pages',
+          '@shared': './src/shared',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
