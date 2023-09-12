995b4dc49bb5c4912d66d3aaba711de1b53e3555
Omar Ramoun <contact@omarramoun.com>
2023-09-12 03:53:25 +0300

    feat(components): add input select dropdown

---
+++ src/components/forms/input-select-all.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {useField} from 'formik';
+
+import {ensureArray} from '@utils';
+
+import type {InputSelectProps} from './input-select';
+import {InputSelect} from './input-select';
+import type {SelectValue} from './input-shared';
+
+const ALL_ID = -1;
+const ALL_LABEL = 'All';
+
+const InputSelectAll = ({
+  formikId,
+  options,
+  ...inputSelectProps
+}: Omit<InputSelectProps, 'value' | 'onValueChange'> & {formikId: string}) => {
+  const [field, _, helpers] = useField<(string | number)[]>(formikId);
+
+  const onValueChange = async (value: SelectValue) => {
+    const newValue = value ? ensureArray(value) : [];
+
+    if (newValue.includes(ALL_ID) && field.value.every((el) => el !== ALL_ID)) {
+      await helpers.setValue([ALL_ID]);
+    } else if (newValue.includes(ALL_ID)) {
+      await helpers.setValue(newValue.filter((el) => el !== ALL_ID));
+    } else if (newValue.length === options.length || newValue.length === 0) {
+      await helpers.setValue([ALL_ID]);
+    } else {
+      await helpers.setValue(newValue);
+    }
+  };
+
+  return (
+    <InputSelect
+      {...inputSelectProps}
+      value={field.value}
+      options={[{label: ALL_LABEL, value: ALL_ID}, ...options]}
+      onValueChange={onValueChange}
+    />
+  );
+};
+
+export {InputSelectAll};
---
+++ src/components/forms/input-select-dropdown.tsx
@@ -0,0 +1,113 @@
+import React, {useRef} from 'react';
+import {Platform, TouchableOpacity, ScrollView} from 'react-native';
+
+import {theme} from '@styles';
+
+import {Button} from '../button/button';
+import {Flex} from '../flex';
+import {Typography} from '../typography';
+
+import {InputCheckbox} from './input-checkbox';
+import type {SelectOptions, SelectValue} from './input-shared';
+
+interface InputSelectDropdownProps {
+  maxHeight?: number;
+  /** The underlying selected value */
+  value: SelectValue;
+
+  /** The passed through options to populate */
+  options: SelectOptions;
+
+  /** Whether multiple can be selected */
+  multiple?: boolean;
+
+  /** The callback for when selecting an item */
+  onSelect: (value: string | number) => void;
+}
+
+const InputSelectDropdown = ({maxHeight, options, value, onSelect, multiple = false}: InputSelectDropdownProps) => {
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  function renderChild(option: any): any {
+    return (
+      <>
+        <Typography numberOfLines={1} textStyle="small" color="darkBlue">
+          {option.label || option.value}
+        </Typography>
+
+        <Flex ml={2}>
+          <InputCheckbox
+            delegateInteraction
+            rounded={!multiple}
+            value={multiple ? Array.isArray(value) && value.includes(option.value) : value === option.value}
+          />
+        </Flex>
+      </>
+    );
+  }
+
+  function renderAndroid(): any {
+    return options.map((option) => {
+      const isSelected = Array.isArray(value) ? value.includes(option.value) : value === option.value;
+      return (
+        <TouchableOpacity
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            padding: 10,
+            alignItems: 'center',
+            ...(isSelected && {backgroundColor: theme.colors.lightGrey1}),
+          }}
+          key={option.value}
+          onPress={() => onSelect(option.value)}>
+          {renderChild(option)}
+        </TouchableOpacity>
+      );
+    });
+  }
+
+  function render() {
+    return options.map((option) => {
+      const isSelected = Array.isArray(value) ? value.includes(option.value) : value === option.value;
+      return (
+        <Button
+          align="space-between"
+          key={option.value}
+          type={isSelected ? 'secondary' : 'tertiary'}
+          onPress={() => onSelect(option.value)}
+          overrideHorizontalPadding={10}>
+          {renderChild(option)}
+        </Button>
+      );
+    });
+  }
+
+  const scrollToFirstSelectedOption = (_scrollViewWidth: number, scrollViewHeight: number) => {
+    const firstSelectedValue = (() => {
+      if (value) {
+        if (typeof value === 'object' && value.length > 0) {
+          return value[0];
+        }
+
+        if (typeof value === 'string' || typeof value === 'number') {
+          return value;
+        }
+
+        return null;
+      }
+
+      return null;
+    })();
+
+    const firstOptionIndex = firstSelectedValue ? options.map((o) => o.value).indexOf(firstSelectedValue) : 0;
+    scrollViewRef.current?.scrollTo((scrollViewHeight / options.length) * firstOptionIndex);
+  };
+
+  return (
+    <ScrollView style={{maxHeight}} ref={scrollViewRef} onContentSizeChange={scrollToFirstSelectedOption}>
+      {Platform.OS === 'android' ? renderAndroid() : render()}
+    </ScrollView>
+  );
+};
+
+export {InputSelectDropdown};
