f3e28f9f5da34930587c96c91dd659fe590b8a0b
Omar Ramoun <contact@omarramoun.com>
2023-09-14 05:58:30 +0300

    refactor(components): pressable

--- src/components/pressable.tsx
+++ src/components/pressable.tsx
@@ -1,17 +1,21 @@
 // TODO: Implement using the new Pressable component from https://reactnative.dev/docs/pressable
+
 import React from 'react';
+import type {ViewStyle} from 'react-native';
 
 import {Box} from './box';
+import type {BoxProps} from './box';
 import {ButtonInteraction} from './button/button-interaction';
 import {Typography} from './typography';
 
-interface PressableProps {
+interface PressableProps extends BoxProps {
   to?: string;
   onPress?: () => void;
   children: React.ReactNode;
+  onPressStyles?: ViewStyle;
 }
 
-const Pressable = ({onPress, to, children}: PressableProps) => (
+const Pressable = ({onPress, to, children, onPressStyles, ...props}: PressableProps) => (
   <ButtonInteraction onPress={onPress} to={to}>
     {({mouseProps, pressed, ...interactionProps}) => (
       <Box
@@ -20,11 +24,17 @@ const Pressable = ({onPress, to, children}: PressableProps) => (
         justifyContent="space-between"
         p={2}
         mb={2}
+        style={pressed ? onPressStyles : {}}
+        {...props}
         {...mouseProps}
         {...interactionProps}>
-        <Typography textStyle="small" color="darkBlue">
-          {children}
-        </Typography>
+        {typeof children === 'string' ? (
+          <Typography textStyle="small" color="darkBlue">
+            {children}
+          </Typography>
+        ) : (
+          children
+        )}
       </Box>
     )}
   </ButtonInteraction>
