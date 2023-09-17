13919eae69c9755c6d795bb646addcf96e37b01d
Omar Ramoun <contact@omarramoun.com>
2023-09-17 09:41:49 +0300

    refactor: add navigation to app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider} from 'react-redux';
 /* import {SafeAreaProvider, useSafeArea} from 'react-native-safe-area-context'; */
 
-import {ModalContainer, ToastContainer, ActionSheetContainer} from '@components';
-import {HomeView} from '@screens';
-import {ThemeProvider} from '@styles';
+import {ModalContainer, ToastContainer, ActionSheetContainer, SafeArea, StatusBar} from '@components';
+import {HomeView, InfoView, FormView} from '@screens';
+import {store} from '@state/store';
+import {ThemeProvider, theme} from '@styles';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider>
-      <AppInner />
+      <Provider store={store}>
+        <AppInner />
+      </Provider>
     </ThemeProvider>
   );
 }
+
+const Stack = createNativeStackNavigator();
 
 const AppInner = () => (
   <ModalContainer displayProps={{noPositionTop: true}}>
     <ActionSheetContainer>
       <ToastContainer>
-        <HomeView />
+        <SafeArea>
+          <StatusBar showHideTransition="slide" animated />
+
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="Home"
+              screenOptions={{
+                headerStyle: {backgroundColor: theme.primaryColor.default},
+                headerTintColor: theme.colors.white,
+                headerTitleAlign: 'center',
+              }}>
+              <Stack.Screen
+                name="Home"
+                component={HomeView}
+                options={{
+                  title: 'Contacts',
+                  headerTitleStyle: {fontWeight: 'bold'},
+                }}
+              />
+
+              <Stack.Screen name="Info" component={InfoView} />
+
+              <Stack.Screen name="Form" component={FormView} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeArea>
       </ToastContainer>
     </ActionSheetContainer>
   </ModalContainer>
