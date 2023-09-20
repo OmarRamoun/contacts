26aabf4909a1631f716a7a501fc0373d31f7f097
Omar Ramoun <contact@omarramoun.com>
2023-09-20 07:20:24 +0300

    refactor(components): use flash list

--- src/components/lists/section-list.tsx
+++ src/components/lists/section-list.tsx
@@ -1,37 +1,82 @@
 import React from 'react';
-import type {SectionListProps as SLProps} from 'react-native';
-import {SectionList as SL} from 'react-native';
+/* import type {SectionListProps as SLProps} from 'react-native'; */
+import {View} from 'react-native';
+
+/* import {SectionList as SL} from 'react-native'; */
+import type {FlashListProps} from '@shopify/flash-list';
+import {FlashList} from '@shopify/flash-list';
 
 import type {ListProps} from './list-shared';
 import {ListFooterComponent, getContainerStyle} from './list-shared';
 
-interface SectionListProps extends Omit<SLProps<any>, 'contentContainerStyle'>, ListProps {
+interface SectionListProps
+  extends Omit<FlashListProps<any>, 'contentContainerStyle' | 'data' | 'renderItem'>,
+    ListProps {
   keyField?: string;
+  sections: {title: string; data: any[]}[];
+  renderSectionHeader: (item: string) => React.ReactElement<any>;
+  renderElement: (item: any, index: number) => React.ReactElement<any>;
 }
+
+const convertToFlatListFormat = (items: SectionListProps['sections']) => {
+  const result: any[] = [];
+
+  items.forEach((section) => {
+    result.push(section.title);
+
+    section.data.forEach((contact: any) => {
+      result.push(contact);
+    });
+  });
+
+  return result as any[];
+};
 
 const SectionList = ({
   sections,
   shouldCenterOnEmpty = true,
   contentContainerStyle = {},
-  renderItem,
+  renderElement,
   canPaginate = false,
   keyField = 'id',
+  renderSectionHeader,
   ...props
-}: SectionListProps) => (
-  <SL
-    sections={sections}
-    removeClippedSubviews
-    contentContainerStyle={getContainerStyle(sections, contentContainerStyle, shouldCenterOnEmpty)}
-    style={{
-      flexGrow: 1,
-      flexShrink: 1,
-      flexBasis: 0,
-    }}
-    renderItem={renderItem}
-    keyExtractor={(item) => `${item[keyField]}`}
-    ListFooterComponent={() => <ListFooterComponent canPaginate={canPaginate} />}
-    {...props}
-  />
-);
+}: SectionListProps) => {
+  const data = convertToFlatListFormat(sections);
+
+  const stickyHeaderIndices = data
+    .map((item, index) => {
+      if (typeof item === 'string') {
+        return index;
+      }
+
+      return null;
+    })
+    .filter((item) => item !== null) as number[];
+
+  return (
+    <View style={{flex: 1}}>
+      <FlashList
+        estimatedItemSize={100}
+        data={data}
+        /* removeClippedSubviews */
+        renderItem={({item, index}) => {
+          if (typeof item === 'string') {
+            // Rendering header
+            return renderSectionHeader(item);
+          }
+
+          return renderElement?.(item, index);
+        }}
+        stickyHeaderIndices={stickyHeaderIndices}
+        getItemType={(item) => (typeof item === 'string' ? 'sectionHeader' : 'row')}
+        contentContainerStyle={getContainerStyle(sections, contentContainerStyle, shouldCenterOnEmpty)}
+        keyExtractor={(item) => `${item[keyField]}`}
+        ListFooterComponent={() => <ListFooterComponent canPaginate={canPaginate} />}
+        {...props}
+      />
+    </View>
+  );
+};
 
 export {SectionList};
--- src/components/touchable-opacity.tsx
+++ src/components/touchable-opacity.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import type {TouchableOpacityProps} from 'react-native';
+import type {TouchableOpacityProps as TOProps} from 'react-native';
 import {TouchableOpacity as TO} from 'react-native';
 
-interface OptionalTouchableOpacityProps extends TouchableOpacityProps {
+interface TouchableOpacityProps extends TOProps {
   children: React.ReactElement;
 }
 
-const TouchableOpacity = ({onPress, children, ...props}: OptionalTouchableOpacityProps) => {
+const TouchableOpacity = ({onPress, children, ...props}: TouchableOpacityProps) => {
   if (onPress) {
     return (
       <TO onPress={onPress} {...props}>
