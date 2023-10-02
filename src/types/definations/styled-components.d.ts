76276c728e983d15a500f6931222a1e92c0ab246
Omar Ramoun <contact@omarramoun.com>
2023-10-02 06:42:17 +0000

    build: tsconfig

--- src/types/definations/styled-components.d.ts
+++ src/types/definations/styled-components.d.ts
@@ -1,8 +1,8 @@
-import {theme} from '../styles';
+import { theme } from '../../styles';
 
 export type AppTheme = typeof theme;
 
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends AppTheme {}
+  export interface DefaultTheme extends AppTheme { }
 }
